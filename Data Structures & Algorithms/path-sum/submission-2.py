71735f7dcceef7cab6f9d13337f3734b445a3dba
nng-thienphu <92291080+nng-thienphu@users.noreply.github.com>
2026-05-18 16:49:44 -0700

    Update: path-sum - submission-2

--- Data Structures & Algorithms/path-sum/submission-2.py
+++ Data Structures & Algorithms/path-sum/submission-2.py
@@ -5,40 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # METHOD: Breadth first search
-    # KEY 1: Queue stores (node, running_sum) pairs
-    #.    this is general problem of BFS 
-    #     if we want to solve this problem we need to add a way to accumulate the sum
-    #.    using: queue = [(node, sum_so_far), ...]
-    #     so when you pop a node, you also get the cummulative sum upn and including that node
-    #     push a child to queue -> push (child, sum_so_far + child.val)
-
-    # KEY 2: Only check running_sum == targetSum 
-    #.          when node.left is None and node.right is None
-    #.    The problem says "root-to-leaf path." So you can't just check the sum at any node — you have to check it only
-    #.     when the node is a leaf (no left AND no right child).
+    # METHOD: Depth-First Search
+    #
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        # edge case:
-        if not root: 
+        # Edge case: empty tree has no root-to-leaf path.
+        if not root:
             return False
         
-        queue = deque() # = [(node1, sum1), (node2, sum2), ... ]
-        queue.append([root, root.val]) 
+        return self.dfs(root, 0, targetSum)
 
-        while queue: 
-            for _ in range(len(queue)) : 
-                node, total = queue.popleft()
+    
+    def dfs(self, node, current_sum, targetSum): 
+        # base case 1: 
+        #.   we don't have to return the path so True, False is enough
+        if not node: 
+            return False
+        
+        # base case 2: a leaf has no left child and no right child
+        if not node.left and not node.right : 
+            if current_sum + node.val == targetSum:
+                return True
+            else: 
+                return False
 
-                # Base case: 
-                # Leaf check: a leaf has no children on EITHER side.
-                if not node.left and not node.right:
-                    if total == targetSum:
-                        return True
+        # recursive call 
+        left = self.dfs(node.left, current_sum+node.val, targetSum)
+        right = self.dfs(node.right, current_sum+node.val, targetSum)
 
-
-                if node.left: 
-                    queue.append([node.left, total + node.left.val]) 
-                if node.right: 
-                    queue.append([node.right, total + node.right.val])
-                
-        return False
+        return left or right 
