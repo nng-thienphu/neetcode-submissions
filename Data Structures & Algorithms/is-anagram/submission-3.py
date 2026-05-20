aa0ab748858be246991ba25e5b0b4299a6309731
nng-thienphu <92291080+nng-thienphu@users.noreply.github.com>
2026-05-20 16:25:59 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    # CLEAN CODE: 
+        # if s[i] in hash_s: 
+        #     hash_s[s[i]] += 1
+        # else: 
+        #     hash_s[s[i]] = 1
+    # CAN BE COLLAPSED INTO
+            # hash_s[s[i]] = hash_s.get(s[i], 0) + 1
+    def isAnagram(self, s: str, t: str) -> bool:
+        hash_s = {}
+        hash_t = {} 
+        if len(s) != len(t) :
+            return False
+
+        for i in range(len(s)): 
+            if s[i] in hash_s: 
+                hash_s[s[i]] += 1
+            else: 
+                hash_s[s[i]] = 1
+
+            if t[i] in hash_t: 
+                hash_t[t[i]] += 1
+            else: 
+                hash_t[t[i]] = 1
+
+        # then compare two hash map 
+        return hash_s == hash_t
