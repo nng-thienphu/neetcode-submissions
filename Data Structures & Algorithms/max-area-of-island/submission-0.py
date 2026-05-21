a48d69a869640ac1c0a52edf72ee9bef1662bb6b
nng-thienphu <92291080+nng-thienphu@users.noreply.github.com>
2026-05-21 03:42:26 -0700

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # setup 
+        ROWS = len(grid) 
+        COLS = len(grid[0]) 
+        max_islands = 0 
+
+        def dfs(r,c): 
+            # Base case: 
+            # 1. out of matrix
+            # 2. pixel is water 
+            if (
+                min(r,c) < 0 or
+                r >= ROWS or  
+                c >= COLS or 
+                grid[r][c] == 0
+            ): 
+                return 0 
+            
+            # Mark visited: change from 1 to 0  
+            grid[r][c] = 0 
+
+            # Dfs logic: move in 4 direction
+            up = dfs(r+1, c)
+            down = dfs(r-1, c) 
+            right = dfs(r, c+1)
+            left = dfs(r, c-1)
+
+            return 1 + up + down + right + left 
+            
+
+        # find the first 1: 
+        for r in range(ROWS): 
+            for c in range(COLS): 
+                if grid[r][c] == 1: 
+                    max_islands = max(max_islands, dfs(r,c))
+
+        return max_islands 
+        
+            
+
