6c96524e4b5065c722a23a2fb044aff55650c4e4
nng-thienphu <92291080+nng-thienphu@users.noreply.github.com>
2026-07-27 17:01:50 -0700

    Add: pascals-triangle - submission-2

---
+++ Data Structures & Algorithms/pascals-triangle/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        tri = [[1] * (r+1) for r in range(numRows)] 
+        result = []
+                
+        for r in range(2, numRows): 
+            for c in range(1, r): 
+                up_left = tri[r-1][c-1]
+                up_right = tri[r-1][c]
+                tri[r][c] = up_left + up_right 
+            
+        
+        return tri
